2c6b024db27b59ceb0f1ee4a7856b8e6d9d3a4cd
David Weinzimmer <dweinzimmer@gmail.com>
2018-08-30 17:46:02 -0400

    Implement gcd() (greatest common divisor)

--- src/recursion.js
+++ src/recursion.js
@@ -316,6 +316,22 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
+  if (x < 0 || y < 0) {
+    return null;
+  }
+  if (x === y) {
+    return x;
+  }
+  if (x === 0) {
+    return y;
+  }
+  if (y === 0) {
+    return x;
+  }
+
+  var remainder = x % y;
+
+  return gcd(y, remainder);
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
