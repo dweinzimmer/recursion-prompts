2803375afb6a4163203282fc342793cbae0a071c
David Weinzimmer <dweinzimmer@gmail.com>
2018-08-30 22:56:45 -0400

    Implement createArray() which creates arrays from strings

--- src/recursion.js
+++ src/recursion.js
@@ -353,6 +353,11 @@ var compareStr = function(str1, str2) {
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
 var createArray = function(str) {
+  if (str.length === 1) {
+    return [str[0]];
+  }
+
+  return [str[0]].concat(createArray(str.substring(1)));
 };
 
 // 17. Reverse the order of an array
