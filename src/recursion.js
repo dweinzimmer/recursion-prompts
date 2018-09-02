51a9480a20652e4572fe63eb1e000dc452918b77
David Weinzimmer <dweinzimmer@gmail.com>
2018-09-02 16:44:50 -0400

    Implement capitalizeWords()

--- src/recursion.js
+++ src/recursion.js
@@ -536,6 +536,11 @@ var nthFibo = function(n) {
 // var words = ['i', 'am', 'learning', 'recursion'];
 // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
 var capitalizeWords = function(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+
+  return [array[0].toUpperCase()].concat(capitalizeWords(array.slice(1)));
 };
 
 // 28. Given an array of strings, capitalize the first letter of each index.
